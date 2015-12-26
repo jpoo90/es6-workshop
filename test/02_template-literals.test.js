1d2ddad8beb324c054d8167e3648ae45457e2c0c
JP Osorio <jpoo90@gmail.com>
2015-12-26 16:11:05 -0500

    Completed string literals

--- test/02_template-literals.test.js
+++ test/02_template-literals.test.js
@@ -1,7 +1,7 @@
 import {expect} from 'chai';
 
-describe('Template Literals', () => {
-  it.skip('should support string interpolation', () => {
+describe.only('Template Literals', () => {
+  it('should support string interpolation', () => {
     // construct a string from the person object using template literal string interpolation
     const person = {
       name: 'Kent C. Dodds',
@@ -15,28 +15,31 @@ describe('Template Literals', () => {
       ]
     };
 
-    const personsFriends = ``;
+    const personsFriends = `${person.name} has ${person.friends.length} friends: ${person.friends.join(', ')}`;
     expect(personsFriends).to.equal(
       'Kent C. Dodds has 6 friends: Brooke Dodds, Matt Zabriskie, Aaron Frost, Dave Geddes, Joe Eames, Ryan Florence'
     );
   });
 
-  it.skip(`should support multi-line strings`, () => {
+  it(`should support multi-line strings`, () => {
     // construct a string with multiple lines without needing escaped newline characters
-    const multiLine = ``;
+    const multiLine = `
+      How cool
+      is this!?
+    `;
     expect(multiLine).to.equal('\n      How cool\n      is this!?\n    ');
   });
 
-  it.skip(`should support string escaping`, () => {
+  it(`should support string escaping`, () => {
     // properly escape a string in a template literal for each of these
-    expect(``).to.equal('Hi\nthere!');
-    expect(``).to.equal('This is `escaped` backtics');
+    expect(`Hi\nthere!`).to.equal('Hi\nthere!');
+    expect(`This is \`escaped\` backtics`).to.equal('This is `escaped` backtics');
   });
 
   describe(`EXTRA CREDIT`, () => {
     // you likely wont often use tagging, but it can be handy!
     describe(`tagging`, () => {
-      it.skip(`should call the tagging function`, () => {
+      it(`should call the tagging function`, () => {
         const noun = 'World';
         const emotion = 'happy';
         const result = tagIt`Hello ${noun}! Are you feeling ${emotion} today?`;
@@ -44,7 +47,9 @@ describe('Template Literals', () => {
 
         function tagIt(literalString, ...interpolatedParts) {
           // implement this function to make the test pass
-          return 'fixme';
+          const fixedSentence = `${literalString[0]}super-cool ${interpolatedParts[0]}${literalString[1]}really ${interpolatedParts[1]}${literalString[2]}`;
+
+          return fixedSentence;
         }
       });
     });
